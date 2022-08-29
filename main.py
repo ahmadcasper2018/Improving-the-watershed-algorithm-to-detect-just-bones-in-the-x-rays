f118667223cffb179ae679f736757b53cf81ef26
casper <ahmadsy88@gmail.com>
2022-08-30 00:47:37 +0300

    Feat: apply auto thresholding

--- main.py
+++ main.py
@@ -11,24 +11,27 @@ from average_filter import (
 from canny import auto_canny
 
 """Apply noise filtering with image integeral"""
-original = cv2.imread(r'test_samples/Lower Extremities-Femur AP-25_01_2016-5_29_55 PM-697.JPEG')
+original = cv2.imread(r'C:\Users\casper\Desktop\project alpha\dataset\Lower Extremities-Knee Lat-1_25_2016-5_40_54 PM-720.JPEG')
 padded_image = image_padding(original)
 integral = make_integeral(padded_image)
 averaged_image = avg_integral(padded_image, integral)
-
-edged_image , l ,u = auto_canny(averaged_image)
-edged_image = edged_image.astype('uint8')
-
-edge = cv2.Canny(averaged_image, l, u)
+#
+# edged_image , l ,u = auto_canny(averaged_image)
+# edged_image = edged_image.astype('uint8')
+ret3, otsu = cv2.threshold(averaged_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+# apply automatic Canny edge detection using the computed median
+lower = otsu.std() * 0.3
+upper = otsu.std()
+edge = cv2.Canny(averaged_image, lower, upper)
 
 # print(averaged_image.shape)
 # lolo = auto_canny(averaged_image)
 # integral_image = integral / integral.max()
-cv2.imshow('original',edged_image)
-cv2.waitKey(0)
+# cv2.imshow('original',edged_image)
+# cv2.waitKey(0)
 #
-cv2.imshow('original',edge)
-cv2.waitKey(0)
+# cv2.imshow('original',edge)
+# cv2.waitKey(0)
 
-# plt.imshow(auto_canny(averaged_image))
-# plt.show()
+plt.imshow(edge,cmap='binary')
+plt.show()
